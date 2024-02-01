71a7acc657a2c0486aaf34e4eab703538db61aeb
DavidKhalid <khaliddavid952@gmail.com>
2024-02-01 19:28:36 +0700

    intialization new branch for seller

--- lib/seller/mainpage_seller.dart/main_page_seller.dart
+++ lib/seller/mainpage_seller.dart/main_page_seller.dart
@@ -8,7 +8,7 @@ class MainPageSeller extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Main Page Seller"),
+        child: Text("Create Main Page Seller"),
       ),
     );
   }
